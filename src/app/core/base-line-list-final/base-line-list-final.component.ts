778eee8033f49d00fa4098d1926c00bf3f7a42b1
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-08 18:13:09 +0530

    Initial commit

---
+++ src/app/core/base-line-list-final/base-line-list-final.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-line-list-final',
+  templateUrl: './base-line-list-final.component.html',
+  styleUrls: ['./base-line-list-final.component.scss']
+})
+export class BaseLineListFinalComponent {
+
+
+checkboxconfig274048={
+type: 'checkbox',
+custom: false,
+checked:true,
+disabed:false,
+};
+
+}
