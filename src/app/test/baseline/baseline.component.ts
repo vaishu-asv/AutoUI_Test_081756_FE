a569a9ceb5c7ce8a284fdacfa33b71135b0268bd
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-08 18:13:43 +0530

    Initial commit

---
+++ src/app/test/baseline/baseline.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-baseline',
+  templateUrl: './baseline.component.html',
+  styleUrls: ['./baseline.component.scss']
+})
+export class BaselineComponent {
+
+
+quickSelectConfig274386={
+isMultiple:false,
+options: [{'id':'1',
+'name': '2 PM',
+},
+],
+};
+
+
+quickSelectConfig274377={
+isMultiple:false,
+options: [{'id':'1',
+'name': '6 AM',
+},
+],
+};
+
+}
